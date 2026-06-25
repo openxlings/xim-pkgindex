59da85625c799429a6be6c00405c2234531616fc
SPeak <speakshen@163.com>
2026-06-25 14:38:36 +0800

    chore(mcpp): bump to 0.0.65 (fix #168 gtest_main; feature-gated dep sources + add --dev) (#315)
    Co-authored-by: sunrisepeak <x.d2learn.org@gmail.com>

--- pkgs/m/mcpp.lua
+++ pkgs/m/mcpp.lua
@@ -39,7 +39,8 @@ package = {
     xpm = {
         linux = {
             url_template = "https://github.com/mcpp-community/mcpp/releases/download/v{version}/mcpp-{version}-linux-x86_64.tar.gz",
-            ["latest"] = { ref = "0.0.64" },
+            ["latest"] = { ref = "0.0.65" },
+            ["0.0.65"] = "XLINGS_RES",
             ["0.0.64"] = "XLINGS_RES",
             ["0.0.63"] = "XLINGS_RES",
             ["0.0.62"] = "XLINGS_RES",
@@ -99,7 +100,8 @@ package = {
             ["0.0.1"] = "XLINGS_RES",
         },
         macosx = {
-            ["latest"] = { ref = "0.0.64" },
+            ["latest"] = { ref = "0.0.65" },
+            ["0.0.65"] = "XLINGS_RES",
             ["0.0.64"] = "XLINGS_RES",
             ["0.0.63"] = "XLINGS_RES",
             ["0.0.62"] = "XLINGS_RES",
@@ -145,7 +147,8 @@ package = {
             ["0.0.16"] = "XLINGS_RES",
         },
         windows = {
-            ["latest"] = { ref = "0.0.64" },
+            ["latest"] = { ref = "0.0.65" },
+            ["0.0.65"] = "XLINGS_RES",
             ["0.0.64"] = "XLINGS_RES",
             ["0.0.63"] = "XLINGS_RES",
             ["0.0.62"] = "XLINGS_RES",
