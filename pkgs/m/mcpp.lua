32d84167308452597dca3c0d72014059faa49929
sunrisepeak <x.d2learn.org@gmail.com>
2026-06-27 20:30:03 +0800

    mcpp: 0.0.68 (dimensional ABI compat — glibc C-lib under libc++ fix)

--- pkgs/m/mcpp.lua
+++ pkgs/m/mcpp.lua
@@ -39,7 +39,8 @@ package = {
     xpm = {
         linux = {
             url_template = "https://github.com/mcpp-community/mcpp/releases/download/v{version}/mcpp-{version}-linux-x86_64.tar.gz",
-            ["latest"] = { ref = "0.0.67" },
+            ["latest"] = { ref = "0.0.68" },
+            ["0.0.68"] = "XLINGS_RES",
             ["0.0.67"] = "XLINGS_RES",
             ["0.0.66"] = "XLINGS_RES",
             ["0.0.65"] = "XLINGS_RES",
@@ -102,7 +103,8 @@ package = {
             ["0.0.1"] = "XLINGS_RES",
         },
         macosx = {
-            ["latest"] = { ref = "0.0.67" },
+            ["latest"] = { ref = "0.0.68" },
+            ["0.0.68"] = "XLINGS_RES",
             ["0.0.67"] = "XLINGS_RES",
             ["0.0.66"] = "XLINGS_RES",
             ["0.0.65"] = "XLINGS_RES",
@@ -151,7 +153,8 @@ package = {
             ["0.0.16"] = "XLINGS_RES",
         },
         windows = {
-            ["latest"] = { ref = "0.0.67" },
+            ["latest"] = { ref = "0.0.68" },
+            ["0.0.68"] = "XLINGS_RES",
             ["0.0.67"] = "XLINGS_RES",
             ["0.0.66"] = "XLINGS_RES",
             ["0.0.65"] = "XLINGS_RES",
