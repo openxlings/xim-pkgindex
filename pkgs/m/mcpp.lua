c24fef97380b794a9c34f0fc5c123bd3e7502a4c
SPeak <speakshen@163.com>
2026-06-29 23:48:24 +0800

    bump(mcpp): 0.0.74 -> 0.0.75 (platform-conditional [target.cfg] dependencies) (#328)
    Mirrored to xlings-res/mcpp (GitHub + GitCode), byte-identical. latest -> 0.0.75.

--- pkgs/m/mcpp.lua
+++ pkgs/m/mcpp.lua
@@ -39,7 +39,8 @@ package = {
     xpm = {
         linux = {
             url_template = "https://github.com/mcpp-community/mcpp/releases/download/v{version}/mcpp-{version}-linux-x86_64.tar.gz",
-            ["latest"] = { ref = "0.0.74" },
+            ["latest"] = { ref = "0.0.75" },
+            ["0.0.75"] = "XLINGS_RES",
             ["0.0.74"] = "XLINGS_RES",
             ["0.0.73"] = "XLINGS_RES",
             ["0.0.72"] = "XLINGS_RES",
@@ -107,7 +108,8 @@ package = {
             ["0.0.1"] = "XLINGS_RES",
         },
         macosx = {
-            ["latest"] = { ref = "0.0.74" },
+            ["latest"] = { ref = "0.0.75" },
+            ["0.0.75"] = "XLINGS_RES",
             ["0.0.74"] = "XLINGS_RES",
             ["0.0.73"] = "XLINGS_RES",
             ["0.0.72"] = "XLINGS_RES",
@@ -161,7 +163,8 @@ package = {
             ["0.0.16"] = "XLINGS_RES",
         },
         windows = {
-            ["latest"] = { ref = "0.0.74" },
+            ["latest"] = { ref = "0.0.75" },
+            ["0.0.75"] = "XLINGS_RES",
             ["0.0.74"] = "XLINGS_RES",
             ["0.0.73"] = "XLINGS_RES",
             ["0.0.72"] = "XLINGS_RES",
