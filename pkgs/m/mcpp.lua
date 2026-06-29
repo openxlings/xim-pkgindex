1c111d2233ddffe4d9fd5c6d9257b818ea19a993
sunrisepeak <x.d2learn.org@gmail.com>
2026-06-29 17:21:24 +0800

    feat(mcpp): bump latest -> 0.0.72 (interface-define propagation; eigen[backend-openblas] closed loop)

--- pkgs/m/mcpp.lua
+++ pkgs/m/mcpp.lua
@@ -39,7 +39,8 @@ package = {
     xpm = {
         linux = {
             url_template = "https://github.com/mcpp-community/mcpp/releases/download/v{version}/mcpp-{version}-linux-x86_64.tar.gz",
-            ["latest"] = { ref = "0.0.70" },
+            ["latest"] = { ref = "0.0.72" },
+            ["0.0.72"] = "XLINGS_RES",
             ["0.0.70"] = "XLINGS_RES",
             ["0.0.68"] = "XLINGS_RES",
             ["0.0.67"] = "XLINGS_RES",
@@ -104,7 +105,8 @@ package = {
             ["0.0.1"] = "XLINGS_RES",
         },
         macosx = {
-            ["latest"] = { ref = "0.0.70" },
+            ["latest"] = { ref = "0.0.72" },
+            ["0.0.72"] = "XLINGS_RES",
             ["0.0.70"] = "XLINGS_RES",
             ["0.0.68"] = "XLINGS_RES",
             ["0.0.67"] = "XLINGS_RES",
@@ -155,7 +157,8 @@ package = {
             ["0.0.16"] = "XLINGS_RES",
         },
         windows = {
-            ["latest"] = { ref = "0.0.70" },
+            ["latest"] = { ref = "0.0.72" },
+            ["0.0.72"] = "XLINGS_RES",
             ["0.0.70"] = "XLINGS_RES",
             ["0.0.68"] = "XLINGS_RES",
             ["0.0.67"] = "XLINGS_RES",
