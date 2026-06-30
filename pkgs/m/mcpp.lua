26dd1df9cdff0562a4f7a35f9be8518419754efe
SPeak <speakshen@163.com>
2026-06-30 12:35:42 +0800

    mcpp: 0.0.80 (bare OS-alias sugar for [target.*]) (#333)
    Mirrored to xlings-res/mcpp (GitHub + GitCode), all 4 platforms GET-verified.
    
    Co-authored-by: sunrisepeak <x.d2learn.org@gmail.com>

--- pkgs/m/mcpp.lua
+++ pkgs/m/mcpp.lua
@@ -39,7 +39,8 @@ package = {
     xpm = {
         linux = {
             url_template = "https://github.com/mcpp-community/mcpp/releases/download/v{version}/mcpp-{version}-linux-x86_64.tar.gz",
-            ["latest"] = { ref = "0.0.79" },
+            ["latest"] = { ref = "0.0.80" },
+            ["0.0.80"] = "XLINGS_RES",
             ["0.0.79"] = "XLINGS_RES",
             ["0.0.78"] = "XLINGS_RES",
             ["0.0.77"] = "XLINGS_RES",
@@ -112,7 +113,8 @@ package = {
             ["0.0.1"] = "XLINGS_RES",
         },
         macosx = {
-            ["latest"] = { ref = "0.0.79" },
+            ["latest"] = { ref = "0.0.80" },
+            ["0.0.80"] = "XLINGS_RES",
             ["0.0.79"] = "XLINGS_RES",
             ["0.0.78"] = "XLINGS_RES",
             ["0.0.77"] = "XLINGS_RES",
@@ -171,7 +173,8 @@ package = {
             ["0.0.16"] = "XLINGS_RES",
         },
         windows = {
-            ["latest"] = { ref = "0.0.79" },
+            ["latest"] = { ref = "0.0.80" },
+            ["0.0.80"] = "XLINGS_RES",
             ["0.0.79"] = "XLINGS_RES",
             ["0.0.78"] = "XLINGS_RES",
             ["0.0.77"] = "XLINGS_RES",
