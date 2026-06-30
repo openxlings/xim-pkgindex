de0d1e217929486f68e42d34800801bffc45e3b3
SPeak <speakshen@163.com>
2026-06-30 10:58:26 +0800

    mcpp: 0.0.79 (workspace-aware mcpp test + build.mcpp cwd fix) (#332)
    Mirrored to xlings-res/mcpp (GitHub + GitCode), all 4 platforms GET-verified.
    
    Co-authored-by: sunrisepeak <x.d2learn.org@gmail.com>

--- pkgs/m/mcpp.lua
+++ pkgs/m/mcpp.lua
@@ -39,7 +39,8 @@ package = {
     xpm = {
         linux = {
             url_template = "https://github.com/mcpp-community/mcpp/releases/download/v{version}/mcpp-{version}-linux-x86_64.tar.gz",
-            ["latest"] = { ref = "0.0.78" },
+            ["latest"] = { ref = "0.0.79" },
+            ["0.0.79"] = "XLINGS_RES",
             ["0.0.78"] = "XLINGS_RES",
             ["0.0.77"] = "XLINGS_RES",
             ["0.0.76"] = "XLINGS_RES",
@@ -111,7 +112,8 @@ package = {
             ["0.0.1"] = "XLINGS_RES",
         },
         macosx = {
-            ["latest"] = { ref = "0.0.78" },
+            ["latest"] = { ref = "0.0.79" },
+            ["0.0.79"] = "XLINGS_RES",
             ["0.0.78"] = "XLINGS_RES",
             ["0.0.77"] = "XLINGS_RES",
             ["0.0.76"] = "XLINGS_RES",
@@ -169,7 +171,8 @@ package = {
             ["0.0.16"] = "XLINGS_RES",
         },
         windows = {
-            ["latest"] = { ref = "0.0.78" },
+            ["latest"] = { ref = "0.0.79" },
+            ["0.0.79"] = "XLINGS_RES",
             ["0.0.78"] = "XLINGS_RES",
             ["0.0.77"] = "XLINGS_RES",
             ["0.0.76"] = "XLINGS_RES",
