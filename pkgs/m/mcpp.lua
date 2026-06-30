8c3d6202dce79fd0192869704975d0a14f2db463
SPeak <speakshen@163.com>
2026-06-30 09:26:24 +0800

    mcpp: 0.0.78 (L3 build.mcpp native build program) (#331)
    Mirrored to xlings-res/mcpp (GitHub + GitCode), all 4 platforms GET-verified.
    
    Co-authored-by: sunrisepeak <x.d2learn.org@gmail.com>

--- pkgs/m/mcpp.lua
+++ pkgs/m/mcpp.lua
@@ -39,7 +39,8 @@ package = {
     xpm = {
         linux = {
             url_template = "https://github.com/mcpp-community/mcpp/releases/download/v{version}/mcpp-{version}-linux-x86_64.tar.gz",
-            ["latest"] = { ref = "0.0.77" },
+            ["latest"] = { ref = "0.0.78" },
+            ["0.0.78"] = "XLINGS_RES",
             ["0.0.77"] = "XLINGS_RES",
             ["0.0.76"] = "XLINGS_RES",
             ["0.0.75"] = "XLINGS_RES",
@@ -110,7 +111,8 @@ package = {
             ["0.0.1"] = "XLINGS_RES",
         },
         macosx = {
-            ["latest"] = { ref = "0.0.77" },
+            ["latest"] = { ref = "0.0.78" },
+            ["0.0.78"] = "XLINGS_RES",
             ["0.0.77"] = "XLINGS_RES",
             ["0.0.76"] = "XLINGS_RES",
             ["0.0.75"] = "XLINGS_RES",
@@ -167,7 +169,8 @@ package = {
             ["0.0.16"] = "XLINGS_RES",
         },
         windows = {
-            ["latest"] = { ref = "0.0.77" },
+            ["latest"] = { ref = "0.0.78" },
+            ["0.0.78"] = "XLINGS_RES",
             ["0.0.77"] = "XLINGS_RES",
             ["0.0.76"] = "XLINGS_RES",
             ["0.0.75"] = "XLINGS_RES",
